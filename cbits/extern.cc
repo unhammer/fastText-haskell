98973deae9dd51e560c341ee753ca688b250d94b
Kevin Brubeck Unhammer <kevin@trigram.no>
2022-11-15 11:19:45 +0100

    wip vector.h

--- cbits/extern.cc
+++ cbits/extern.cc
@@ -41,6 +41,44 @@ int get_dimension(fasttext::FastText* fasttext) {
     return fasttext->getDimension();
 }
 
+// vector.h
+const fasttext::real* vector_data(const fasttext::Vector* self) {
+    return self->data();
+}
+fasttext::real vector_at(const fasttext::Vector* self, int64_t i) {
+    return (*self)[i];
+}
+int64_t vector_size(fasttext::Vector* self) {
+    return self->size();
+}
+void vector_zero(fasttext::Vector* self) {
+    self->zero();
+}
+void vector_scale(fasttext::Vector* self, fasttext::real a) {
+    self->mul(a);
+}
+fasttext::real vector_norm(const fasttext::Vector* self) {
+    return self->norm();
+}
+void vector_addVector(fasttext::Vector* self, const fasttext::Vector* source) {
+    self->addVector(*source);
+}
+void vector_addVectorScaled(fasttext::Vector* self, const fasttext::Vector* source, fasttext::real s) {
+    self->addVector(*source, s);
+}
+void vector_addRow(fasttext::Vector* self, const fasttext::Matrix* A, int64_t i) {
+    self->addRow(*A, i);
+}
+void vector_addRowScaled(fasttext::Vector* self, const fasttext::Matrix* A, int64_t i, fasttext::real a) {
+    self->addRow(*A, i, a);
+}
+void vector_mul(fasttext::Vector* self, const fasttext::Matrix* A, const fasttext::Vector* vec) {
+    self->mul(*A, *vec);
+}
+int64_t vector_argmax(fasttext::Vector* self) {
+    return self->argmax();
+}
+
 /* TODO: vector constructor + printer, so we can fasttext.getWordVector */
 
 }
